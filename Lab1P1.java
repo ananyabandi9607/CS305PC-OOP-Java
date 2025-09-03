779cb77951aef7e50f83ca325c7ad29de73992d5
ananyabandi9607 <ananya9607@gmail.com>
2025-09-03 11:29:42 +0530

    Add files via upload

---
+++ Lab1P1.java
@@ -0,0 +1,69 @@
+class Lab1P1{
+    public static void main(String...args){
+    byte b = 15;
+    Byte bb = b; //autoboxing
+    
+      //int x = 200;
+      //byte b = (byte)x;
+      
+      System.out.println(Byte.MAX_VALUE);
+      System.out.println(bb.MIN_VALUE);
+      System.out.println(bb.SIZE);
+      System.out.println(bb.TYPE);
+      System.out.println(bb.BYTES);
+      
+      
+      
+      System.out.println("-----------------SHORT---------------");
+      short s = 15;
+      Short ss = s;
+      System.out.println(Short.MAX_VALUE);
+      System.out.println(ss.MIN_VALUE);
+      System.out.println(ss.SIZE);
+      System.out.println(ss.TYPE);
+      System.out.println(ss.BYTES);
+      
+      
+      System.out.println("---------------- INTEGER----------------");
+      int i = 15;
+      Integer ii = i;
+      System.out.println(Integer.MAX_VALUE);
+      System.out.println(ii.MIN_VALUE);
+      System.out.println(ii.SIZE);
+      System.out.println(ii.TYPE);
+      System.out.println(ii.BYTES);
+      
+      
+      
+     System.out.println("---------------- LONG----------------") ;
+      long l = 15;
+      Long ll = l;
+      System.out.println(Long.MAX_VALUE);
+      System.out.println(ll.MIN_VALUE);
+      System.out.println(ll.SIZE);
+      System.out.println(ll.TYPE);
+      System.out.println(ll.BYTES);
+      
+      
+
+   System.out.println("----------------BOOLEAN----------------");
+      boolean z = true;
+      Boolean zz = z;
+      //System.out.println(Boolean.MAX_VALUE);
+      //System.out.println(zz.MIN_VALUE);
+      //System.out.println(zz.SIZE);
+      System.out.println(zz.TYPE);
+     // System.out.println(zz.BYTE);
+    
+    
+    
+    
+    
+    
+    
+    
+    
+       
+   }
+   
+}   
